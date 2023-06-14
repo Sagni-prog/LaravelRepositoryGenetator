0578922cc91f77e2116a0bd877d2738506bd4140
Sagni Alemayehu <sagnialemayehu69@gmail.com>
2023-06-14 22:48:05 +0300

    ### service provider adder to config ###

--- src/MakeRepositoryCommand.php
+++ src/MakeRepositoryCommand.php
@@ -40,14 +40,22 @@ class MakeRepositoryCommand extends Command
           mkdir(app_path('Repositories'));
       }
       
+      
+      
       if(array_key_exists(1, $des)){
          $this->createRepositoryFile($des[1],$des[0]);
          $interfacePath = app_path('Repositories'.DIRECTORY_SEPARATOR.$des[0].DIRECTORY_SEPARATOR.$des[1].'Interface.php');
          $this->createInterfaceFile($des[1],$interfacePath,'App\\Repositories\\'.$des[0]);
+         $this->createServiceProviderFile($des[1],$des[0]);
+         $this->addServiceProviderToConfig($des[1],$des[0]);
+
       }else{
          $this->createRepositoryFile($name);
          $interfacePath = app_path('Repositories'.DIRECTORY_SEPARATOR.$name.'Interface.php');
          $this->createInterfaceFile($name,$interfacePath,'App\\Repositories');
+         $this->createServiceProviderFile($name);
+         $this->addServiceProviderToConfig($name);
+
       }
       
     }
@@ -62,6 +70,11 @@ class MakeRepositoryCommand extends Command
       
       return file_get_contents(__DIR__.'/stubs/interface.stub');
 
+    }
+    
+    private function getStub($path){
+      
+      return file_get_contents($path);
     }
     
     private function getPath(){
@@ -105,17 +118,58 @@ class MakeRepositoryCommand extends Command
        file_put_contents($path, $content);
     }
     
-   private function addServiceProviderToConfig(){
+    private function createServiceProviderFile($name, $folder = null){
+        
+        $stub = $this->getStub(__DIR__.'/stubs/serviceProvider.stub');
+        
+        $content = str_replace('{{ repositoryInterfaceClass }}',$name."Interface",$stub);
+        $content = str_replace('{{ class }}',$name."ServiceProvider",$content);
+        $content = str_replace('{{ repositoryClass }}',$name,$content);
+        
+        if($folder !== null){
+           
+         if(!is_dir(app_path('Repositories'.DIRECTORY_SEPARATOR.$folder))){
+         
+          mkdir(app_path('Repositories'.DIRECTORY_SEPARATOR.$folder));
+         }
+          $content = str_replace('{{ namespace }}',"App\Repositories"."\\".$folder,$content);
+          $content = str_replace('{{ repository }}',"App\Repositories"."\\".$folder."\\".$name,$content);
+          $content = str_replace('{{ repositoryinterface }}',"App\Repositories"."\\".$folder."\\".$name."Interface",$content);
+          
+       }
+       else{
+       
+         $content = str_replace('{{ namespace }}',"App\Repositories",$content);
+         $content = str_replace('{{ repository }}',"App\Repositories"."\\".$name,$content);
+         $content = str_replace('{{ repositoryinterface }}',"App\Repositories"."\\".$name."Interface",$content);
+       }
+       
+       $path = app_path('Repositories'.DIRECTORY_SEPARATOR.$folder.DIRECTORY_SEPARATOR.$name.'ServiceProvider.php');
+       file_put_contents($path,$content);
+      
+    }
+    
+   private function addServiceProviderToConfig($name,$folder=null){
        
       $file = base_path('config'.DIRECTORY_SEPARATOR.'app.php');
     
       $content = file_get_contents($file);
-      $classToConcatenate = "\t".'Sagni\Repository\RepositoryInterfaces::class,'."\n";
+      
+      if($folder !== null){
+         
+         $classToConcatenate = "\t".'Sagni\\Repository\\'.$folder.'\\'.$name.'ServiceProvider::class,'."\n";
+      }
+      else {
+         $classToConcatenate = "\t".'Sagni\\Repository\\'.$name.'ServiceProvider::class,'."\n";
+      }
+      
+     
       $pattern = '/(\'providers\'\s*=>\s*\[.*?\]\s*,)/s';
       preg_match($pattern, $content, $matches);
       $closingBracket = strrpos($matches[0], ']');
       $newContent = substr_replace($matches[0], $classToConcatenate, $closingBracket, 0);
-      $content = str_replace($matches[0], $newContent, $content);
+      // $content = str_replace($matches[0], $newContent, $content);
+      echo $newContent;
       
    } 
     
